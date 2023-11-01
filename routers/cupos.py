222e9139ebdd6256bb5f0b1bbbf019ff23bc9a0b
Adrian Oropeza <adrianantoniool@hotmail.com>
2023-11-01 16:17:55 -0400

    Ultima version

--- routers/cupos.py
+++ routers/cupos.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, HTTPException
 from fastapi_utils.tasks import repeat_every 
+from fastapi import APIRouter, HTTPException
+import random
 
 router = APIRouter()
 
@@ -20,11 +21,12 @@ def init_data():
 @router.post("/validador_cupos")
 async def validador_cupos(data: dict):
 	global cupos
+	numero_aleatorio = random.randint(1, 20)
 	tipo = data.get("tipo")
     # Verificar si hay cupos disponibles
-	if cupos["GC"]> 0 and tipo == "GC":
+	if cupos["GC"]> 0 and tipo == "GC" and numero_aleatorio == 20:
 		cupos["GC"]-= 1
-	elif cupos["PO"] > 0 and tipo == "PO":
+	elif cupos["PO"] > 0 and tipo == "PO" and numero_aleatorio == 20:
 		cupos["PO"] -= 1
 	else:
 		raise HTTPException(status_code=400, detail="0")
--- test-main.py
+++ test-main.py
@@ -1,14 +1,14 @@
 import requests
 import concurrent.futures
 
-url = "https://cupos-available.onrender.com/validador_cupos"
+url = "http://127.0.0.1:8000/validador_cupos"
 
 def realizar_solicitud(i):
     response = requests.post(url, json={"correo": f"usuario{i}@ejemplo.com", "tipo": "PO"})
     print(f"Solicitud {i}: {response.status_code}")
 
 # Número total de solicitudes
-total_solicitudes = 20
+total_solicitudes = 100
 
 # Realizar solicitudes de forma concurrente
 with concurrent.futures.ThreadPoolExecutor() as executor:
